0ad825ac4c6afdfd149002c84b877fd74b54ca83
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-12 20:20:10 -0300

    testing nodejs

--- utils/routes.js
+++ utils/routes.js
@@ -20,27 +20,26 @@ class Routes{
 
 		this.app.post('/usernameCheck', function (request, response) {
 
-			// if (request.body.username === "") {
-			// 	response.status(412).json({
-			// 		error : true,
-			// 		message : `username cant be empty.`
-			// 	});
-			// } else {
-			// 	helper.userNameCheck( {
-			// 		username : request.body.username.toLowerCase()
-			// 	}, (count)=>{
+			if (request.params.username === "") {
+				response.status(412).json({
+					error : true,
+					message : `username cant be empty.`
+				});
+			} else {
+				helper.userNameCheck( {
+					username : request.params.username.toLowerCase()
+				}, (count)=>{
 
-			// 		let result = {};
+					let result = {};
 					
-			// 		if (count > 0) {
-			// 			result.error = true;
-			// 		} else {
-			// 			result.error = false;
-			// 		}
-			// 		response.status(200).json(result);
-			// 	});
-			// }
-			response.status(200).json(request.body)
+					if (count > 0) {
+						result.error = true;
+					} else {
+						result.error = false;
+					}
+					response.status(200).json(result);
+				});
+			}
 
 		});
 
