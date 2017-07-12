02d2b22e6a8ff858f3a6e507d80a41ef274134d4
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-12 19:51:08 -0300

    updated version

--- utils/routes.js
+++ utils/routes.js
@@ -18,29 +18,29 @@ class Routes{
 	/* creating app Routes starts */
 	appRoutes(){
 
-		// this.app.post('/usernameCheck',(request,response) =>{
+		this.app.post('/usernameCheck',(request,response) =>{
 
-		// 	if (request.body.username === "") {
-		// 		response.status(412).json({
-		// 			error : true,
-		// 			message : `username cant be empty.`
-		// 		});
-		// 	} else {
-		// 		helper.userNameCheck( {
-		// 			username : request.body.username.toLowerCase()
-		// 		}, (count)=>{
+			if (request.body.username === "") {
+				response.status(412).json({
+					error : true,
+					message : `username cant be empty.`
+				});
+			} else {
+				helper.userNameCheck( {
+					username : request.body.username.toLowerCase()
+				}, (count)=>{
 
-		// 			let result = {};
+					let result = {};
 					
-		// 			if (count > 0) {
-		// 				result.error = true;
-		// 			} else {
-		// 				result.error = false;
-		// 			}
-		// 			response.status(200).json(result);
-		// 		});
-		// 	}
-		// });
+					if (count > 0) {
+						result.error = true;
+					} else {
+						result.error = false;
+					}
+					response.status(200).json(result);
+				});
+			}
+		});
 
 		// this.app.post('/registerUser',(request,response) =>{
 
