9820ebcb8c0d2b262820e04b591379a55643302b
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-12 16:20:07 -0300

    change server.js

--- server.js
+++ server.js
@@ -1,61 +1,73 @@
-/*
-* Real time private chatting app using Angular 2, Nodejs, mongodb and Socket.io
-* @author Shashank Tiwari
-*/
+// /*
+// * Real time private chatting app using Angular 2, Nodejs, mongodb and Socket.io
+// * @author Shashank Tiwari
+// */
 
-'use strict';
+// 'use strict';
 
-const express = require("express");
-const http = require('http');
-const socketio = require('socket.io');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// const express = require("express");
+// const http = require('http');
+// const socketio = require('socket.io');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
 
-const socketEvents = require('./utils/socket'); 
-const routes = require('./utils/routes'); 
-const config = require('./utils/config'); 
+// const socketEvents = require('./utils/socket'); 
+// const routes = require('./utils/routes'); 
+// const config = require('./utils/config'); 
 
 
-class Server{
+// class Server{
 
-    constructor(){
-        this.port =  process.env.PORT || 1337;
-        // this.host = `localhost`;
+//     constructor(){
+//         this.port =  process.env.PORT || 4000;
+//         this.host = `localhost`;
         
-        this.app = express();
-        this.http = http.Server(this.app);
-        this.socket = socketio(this.http);
-    }
+//         this.app = express();
+//         this.http = http.Server(this.app);
+//         this.socket = socketio(this.http);
+//     }
 
-    appConfig(){        
-        this.app.use(
-            bodyParser.json()
-        );
-        this.app.use(
-        	cors()
-        );
-        new config(this.app);
-    }
+//     appConfig(){        
+//         this.app.use(
+//             bodyParser.json()
+//         );
+//         this.app.use(
+//         	cors()
+//         );
+//         new config(this.app);
+//     }
 
-    /* Including app Routes starts*/
-    includeRoutes(){
-        new routes(this.app).routesConfig();
-        new socketEvents(this.socket).socketConfig();
-    }
-    /* Including app Routes ends*/  
+//     /* Including app Routes starts*/
+//     includeRoutes(){
+//         new routes(this.app).routesConfig();
+//         new socketEvents(this.socket).socketConfig();
+//     }
+//     /* Including app Routes ends*/  
 
-    appExecute(){
+//     appExecute(){
 
-        this.appConfig();
-        this.includeRoutes();
+//         this.appConfig();
+//         this.includeRoutes();
 
-        this.http.createServer(function (req, res) {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Hello World\n');
-        }).listen(this.port);
-    }
+//         this.http.listen(this.port, this.host, () => {
+//             console.log(`Listening on http://${this.host}:${this.port}`);
+//         });
+//     }
 
-}
+// }
 
-const app = new Server();
-app.appExecute();
+// const app = new Server();
+// app.appExecute();
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
