2ebf6f6476f8c4fb00dd44f6273cf3022273fa75
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-12 20:29:46 -0300

    testing nodejs

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,6 @@
-/*
-* Real time private chatting app using Angular 2, Nodejs, mongodb and Socket.io
-* @author Shashank Tiwari
-*/
- 
 "use strict";
 /*requiring mongodb node modules */
-const mongodb = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
 const assert = require('assert');
 
 class Db{
@@ -13,10 +8,11 @@ class Db{
 	constructor(){
 		this.mongoClient = mongodb.MongoClient;
 		this.ObjectID = mongodb.ObjectID;
+		this.mongoURL = `mongodb://mongojubs:ZrOeAeyb6mds1zymz6PVDmz4u0QIiSa2ITpsoxtcoQeMWOP0BovQHohoZhd69PRuhevtHZ29rxzIcGQGRPIAFQ==@mongojubs.documents.azure.com:10255/?ssl=true`;
 	}
 
 	onConnect(callback){
-		this.mongoClient.connect("mongodb://mongojubs:ZrOeAeyb6mds1zymz6PVDmz4u0QIiSa2ITpsoxtcoQeMWOP0BovQHohoZhd69PRuhevtHZ29rxzIcGQGRPIAFQ==@mongojubs.documents.azure.com:10255/?ssl=true", function (err, db) {
+		this.mongoClient.connect(this.mongoURL, (err, db) => {
 			assert.equal(null, err);
 			callback(db,this.ObjectID);
 		});
