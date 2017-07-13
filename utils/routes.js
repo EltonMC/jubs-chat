4905097fe802e4222e0fbb52f14f7fb32635a26b
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-13 10:22:28 -0300

    testing nodejs

--- utils/routes.js
+++ utils/routes.js
@@ -36,7 +36,7 @@ class Routes{
 				// 	} else {
 				// 		result.error = false;
 				// 	}
-				response.status(200).json(request.body);
+				response.status(200).json(request);
 				// });
 			}
 		});
