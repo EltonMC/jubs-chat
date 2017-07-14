638e3bc40a2072bce1905adf21541dc15755753b
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-14 20:17:29 -0300

    testing nodejs

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ class Server{
             bodyParser.json()
         );
         this.app.use(function (req, res, next) {
-            res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
+            res.setHeader('Access-Control-Allow-Origin', "*");
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
             res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
             next();
