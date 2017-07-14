dc7c2dbc512ea108ef40ecbc1ff4ae88ddaa832b
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-14 20:18:36 -0300

    testing nodejs

--- server.js
+++ server.js
@@ -26,12 +26,6 @@ class Server{
         this.app.use(
             bodyParser.json()
         );
-        this.app.use(function (req, res, next) {
-            res.setHeader('Access-Control-Allow-Origin', "*");
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-            next();
-        });
         this.app.use(
         	cors()
         );
