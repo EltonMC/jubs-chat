9e8a78b693a56769047b31447eedbaa13da97469
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-16 10:48:11 -0300

    updated api chat

--- utils/routes.js
+++ utils/routes.js
@@ -57,27 +57,27 @@ class Routes{
 	            registrationResponse.message = `id cant be empty.`;
 	            response.status(412).json(registrationResponse);
 			}else {
-				// helper.userCheck({idUser: data.idUser}, (count) =>{
-				// 	let result = {};
+				helper.userCheck({idUser: data.idUser}, (count) =>{
+					// let result = {};
 
-				// 	if (count > 0) {
-				// 		registrationResponse.error = true;
-				// 		registrationResponse.message = `id in use`;
-				// 		response.status(200).json(registrationResponse);
-				// 	} else {
-				// 		helper.registerUser( data, (error,result)=>{
-				// 			if (error) {
-				// 				registrationResponse.error = true;
-				// 				registrationResponse.message = `Server error.`;
-				// 				response.status(404).json(registrationResponse);
-				// 			}else{
-				// 				registrationResponse.error = false;
-				// 				registrationResponse.message = `User registration successful.`;
+					// if (count > 0) {
+					// 	registrationResponse.error = true;
+					// 	registrationResponse.message = `id in use`;
+					// 	response.status(200).json(registrationResponse);
+					// } else {
+					// 	helper.registerUser( data, (error,result)=>{
+					// 		if (error) {
+					// 			registrationResponse.error = true;
+					// 			registrationResponse.message = `Server error.`;
+					// 			response.status(404).json(registrationResponse);
+					// 		}else{
+					// 			registrationResponse.error = false;
+					// 			registrationResponse.message = `User registration successful.`;
 								response.status(200).json(registrationResponse);
-				// 			}
-				// 		});					
-				// 	}
-				// });
+					// 		}
+					// 	});					
+					// }
+				});
 			}
 		});
 
