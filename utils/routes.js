a92d7e4bfa73561d5f2ee12b83dae8a40a160e05
Elton Carvalho <eltonmc@dcomp.ufs.br>
2018-01-11 11:11:46 -0300

    debug api chat

--- utils/routes.js
+++ utils/routes.js
@@ -21,35 +21,37 @@ class Routes{
 				status: 'N'
 			}
 
+			response.status(200).json(data);
+			
 			let registrationResponse = {}
 
-			if (data.idUser === ''){
-	            registrationResponse.error = true;
-	            registrationResponse.message = `id cant be empty.`;
-	            response.status(412).json(registrationResponse);
-			}else {
-				helper.userCheck({idUser: data.idUser}, (count) =>{
-					let result = {};
 
-					if (count > 0) {
-						registrationResponse.error = true;
-						registrationResponse.message = `id in use`;
-						response.status(200).json(registrationResponse);
-					} else {
-						helper.registerUser( data, (error,result)=>{
-							if (error) {
-								registrationResponse.error = true;
-								registrationResponse.message = `Server error.`;
-								response.status(404).json(registrationResponse);
-							}else{
-								registrationResponse.error = false;
-								registrationResponse.message = `User registration successful.`;
-								response.status(200).json(registrationResponse);
-							}
-						});					
-					}
-				});
-			}
+			// if (data.idUser === ''){
+	        //     registrationResponse.error = true;
+	        //     registrationResponse.message = `id cant be empty.`;
+	        //     response.status(412).json(registrationResponse);
+			// }else {
+			// 	helper.userCheck({idUser: data.idUser}, (count) =>{
+			// 		let result = {};
+			// 		if (count > 0) {
+			// 			registrationResponse.error = true;
+			// 			registrationResponse.message = `id in use`;
+			// 			response.status(200).json(registrationResponse);
+			// 		} else {
+			// 			helper.registerUser( data, (error,result)=>{
+			// 				if (error) {
+			// 					registrationResponse.error = true;
+			// 					registrationResponse.message = `Server error.`;
+			// 					response.status(404).json(registrationResponse);
+			// 				}else{
+			// 					registrationResponse.error = false;
+			// 					registrationResponse.message = `User registration successful.`;
+			// 					response.status(200).json(registrationResponse);
+			// 				}
+			// 			});					
+			// 		}
+			// 	});
+			// }
 		});
 
 		this.app.get('/users/:id',(request, response) => {
