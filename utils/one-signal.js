cfae27bc71885441eaf63d9f43dbe0a60eaa17a3
Elton Carvalho <eltonmc@dcomp.ufs.br>
2018-01-19 13:25:51 -0300

    fix onesignal

--- utils/one-signal.js
+++ utils/one-signal.js
@@ -1,32 +1,37 @@
 var request = 	require('request');
 
-var sendMessage = function(device, message){
-	var restKey = 'ZTYwMDgzM2QtM2U0Yi00ZmNlLWFhMWItNDJjZTBkNTBjOGQ0';
-	var appID = 'c6e40e76-770a-459e-a067-b25db23ec5df';
-	request(
-		{
-			method:'POST',
-			uri:'https://onesignal.com/api/v1/notifications',
-			headers: {
-				"authorization": "Basic "+restKey,
-				"content-type": "application/json"
-			},
-            json: true,
-            rejectUnauthorized: false,
-            requestCert: false,
-			body:{
-				'app_id': appID,
-				'contents': {en: message},
-				'include_player_ids': Array.isArray(device) ? device : [device]
-			}
-		},
-		function(error, response, body) {
-			if(!body.errors){
-				console.log(body);
-			}else{
-				console.error('Error:', body.errors);
-			}
-			
-		}
-	);
+class Onesignal{
+
+    sendMessage = function(device, message){
+        let restKey = 'ZTYwMDgzM2QtM2U0Yi00ZmNlLWFhMWItNDJjZTBkNTBjOGQ0';
+        let appID = 'c6e40e76-770a-459e-a067-b25db23ec5df';
+        request(
+            {
+                method:'POST',
+                uri:'https://onesignal.com/api/v1/notifications',
+                headers: {
+                    "authorization": "Basic "+restKey,
+                    "content-type": "application/json"
+                },
+                json: true,
+                rejectUnauthorized: false,
+                requestCert: false,
+                body:{
+                    'app_id': appID,
+                    'contents': {en: message},
+                    'include_player_ids': Array.isArray(device) ? device : [device]
+                }
+            },
+            function(error, response, body) {
+                if(!body.errors){
+                    console.log(body);
+                }else{
+                    console.error('Error:', body.errors);
+                }
+                
+            }
+        );
+    }
 }
+
+module.exports = new Onesignal();
